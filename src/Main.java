6bc62991eaac6581fa02fd79805a2c97516a9db0
Peter <szpeto12@gmail.com>
2025-03-02 17:32:05 +0100

    Final working version v2

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Main {
+
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+        s.close();
+        
+	        HashSet<String> pairs = new HashSet<>();
+	        int count = 0;
+		    for(int i = 0; i<t; i++) {
+	            
+	            String pair1 = pair_left[i] + " " + pair_right[i];
+	            String pair2 = pair_right[i] + " " + pair_left[i];
+	            if(!pairs.contains(pair1)) {
+		       	    pairs.add(pair1);
+	                pairs.add(pair2);
+		     	    count++;
+		       	    System.out.println(count);
+	            } else {
+	                System.out.println(count);
+	            }
+		    	  
+		    }
+
+    }
+}
